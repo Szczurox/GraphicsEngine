06f4dc4aa21c4d5c3cb292242d410a8f7b3d048a
Szczurox <58273015+Szczurox@users.noreply.github.com>
2023-07-25 15:40:23 +0200

    Changed default demo in main and removed weird temp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 
-#include "demo/maze.hpp"
+#include "demo/tictactoe.hpp"
 
 int WINAPI WinMain(_In_ HINSTANCE curInst, _In_opt_ HINSTANCE prevInst, _In_ PSTR cmdLine, _In_ INT cmdCount) {
-	MazeDemoMain(curInst, prevInst, cmdLine, cmdCount);
+	TicTacToeDemoMain(curInst, prevInst, cmdLine, cmdCount);
 }
